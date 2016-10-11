ce0c64d0061e94fac3dfa6fa9faab3ae859071b9
Igor Wiedler <igor@travis-ci.org>
2016-10-11 19:52:20 +0200

    add a start-sso process similar to start-nginx

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: start-stunnel ./run-workwebui.sh
+web: ./start-sso -f start-stunnel ./run-workwebui.sh
--- run-workwebui.sh
+++ run-workwebui.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-sso -listen ":$PORT" \
-    -upstream 'http://127.0.0.1:5000' \
-    -app "$SSO_APP_PUBLIC_URL" \
-    -static-path 'sso-assets/static'
-    -template-path 'sso-assets/template'
-    -encryption-key "$SSO_ENCRYPTION_KEY" \
-    -csrf-key "$SSO_CSRF_KEY" \
-    -authorized-users "$SSO_AUTHORIZED_USERS" &
-workwebui -redis $REDIS_URL -ns cloud-brain:worker -listen :5000
+workwebui -redis $REDIS_URL -ns cloud-brain:worker -listen 127.0.0.1:5000
